4ec330a280f62c6e756aacb10956ab45ae6ba753
Daniel Gonzalez <219147853+Taloc-Dev@users.noreply.github.com>
2026-02-12 16:29:01 +0100

    feat: Add recolor grid feature with mouse control

--- etchAndSketch.js
+++ etchAndSketch.js
@@ -2,5 +2,18 @@ const gridContainer = document.querySelector(".grid-container");
 
 for(let i = 0; i < 256; i++){
     const nextSquare = document.createElement("div");
+    nextSquare.classList.add("square");
     gridContainer.appendChild(nextSquare);
 }
+
+function DrawSquareOnMouseEnter(){ 
+    gridContainer.addEventListener("mouseover", (e) => {
+        let target = e.target;
+
+        if(target.classList.contains("square")){
+            target.style.backgroundColor = "black";
+        }
+    });
+}
+
+DrawSquareOnMouseEnter();
